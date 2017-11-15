6087347c78b45d4ca7a0fee7a2b6c8925d3c252a
shantanu <mailtoshants@gmail.com>
2017-11-15 13:53:46 +0530

    some impl

--- interface_eg.go
+++ interface_eg.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"math"
 
-)
+	)
 
 type vertex struct {
 	x float64
@@ -34,18 +34,37 @@ func (p *person) String() string {
 	return fmt.Sprintf("%v %v years", p.name, p.age)
 }
 
+func (*person) PrintHello() string {
+	return fmt.Sprintf("Hello")
+}
+
+func (p *person) Abs() float64 {
+	return float64(p.age)
+}
+
 type IPAddr [4]byte
 // y by value and not pointer
 func (ip IPAddr) String() string {
 	return fmt.Sprintf("%v.%v.%v.%v",ip[0],ip[1],ip[2],ip[3])
 
 }
+
+func testEmptyInter(a interface{}){
+
+	fmt.Println("Hello empty")
+}
+
+func implAbs(abser Abser) float64 {
+	return abser.Abs()
+}
+
 func main() {
  v:=vertex{3,4}
  fmt.Println(v.Abs())
 
 	a := person{"Arthur Dent", 42}
 	fmt.Println(a)
+	fmt.Println(a.PrintHello())
 	hosts := map[string]IPAddr{
 		"loopback":  {127, 0, 0, 1},
 		"googleDNS": {8, 8, 8, 8},
@@ -54,5 +73,10 @@ func main() {
 		fmt.Printf("%v: %v\n", name, ip)
 	}
 
+	testEmptyInter(v)
+	testEmptyInter(a)
+
+	fmt.Println(implAbs(&v))
+	fmt.Println(implAbs(&a))
  	}
 
